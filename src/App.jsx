1855837b786cc98db114bb694844894b6ee3b19b
Julian Cardia <juliancardia87@gmail.com>
2024-09-03 10:55:31 -0300

    empezando a migrag los archivos del proyecto etapa 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Inicio from './pages/inicio'
-import Alta from './pages/Alta'
-import Nosotros from './pages/Nosotros'
-import Contacto from './pages/Contacto'
-import NoEncontrado from './pages/NoEncontrado'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Rutas from "./routes/Rutas";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-   < BrowserRouter>
-   
-    <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/alta" element={<Alta />} />
-          <Route path="/nosotros" element={<Nosotros />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="*" element={<NoEncontrado />} />
+    <BrowserRouter>
+    
+      <Navbar />
 
-    </Routes>
-   
-   </BrowserRouter>
-  )
-}
+      <Header />
 
-export default App
+      <Rutas />
+
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
