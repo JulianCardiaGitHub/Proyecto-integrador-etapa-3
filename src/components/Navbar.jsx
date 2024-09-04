37acc6567082287de128b79db1f5bda74dede9f9
Julian Cardia <juliancardia87@gmail.com>
2024-09-04 10:35:29 -0300

    refactorizando navbar y header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,24 @@
+import menuItems from "../constants/menuItems";
+
+
 
 
 const Navbar = () => {
   return (
- <div>Navbar</div>
-  
-  )
-}
+    <nav className="nav-bar">
+      <ul className="nav-bar__nav-list">
 
-export default Navbar
+        {menuItems.map((item) => (
+          <li className="nav-bar__nav-item" key={item.id}>
+            <a href={item.ruta} className="nav-bar__nav-link">
+              {item.nombre}
+            </a>
+          </li>
+        ))}
+
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
