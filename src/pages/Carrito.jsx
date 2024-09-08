7a58aabdba52fb38eaeadd01f03d23c7f9636233
Julian Cardia <juliancardia87@gmail.com>
2024-09-08 15:28:09 -0300

    listadocarrito y itemcarrito

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,21 +1,19 @@
-import { useContext } from "react"
 import useTitulo from "../hooks/useTitulo"
-import CarritoContext from "../context/CarritoContext"
+import ListadoCarrito from "../components/ListadoCarrito"
 
 
 const Carrito = () => {
 
   useTitulo('Carrito')
  
-    
-const { carrito, eliminarProductoAlCarritoContext, guardarCarritoContext  } = useContext(CarritoContext)
-
+  
 
   return (
-    
+    <>
    <h1>Productos en el carrito</h1>
    
-    
+    <ListadoCarrito />
+    </>
   )
 }
 
